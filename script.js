4e8bd273b83d3d741b1825d92293102b0d07f9cf
jhonsfranky17 <franklinjettyjohnson6@gmail.com>
2024-01-03 13:43:04 +0530

    Style: add hamburger

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let links = document.getElementById("links");
 let hero = document.getElementById("hero");
 let footer = document.getElementById("footer");
 let projects = document.getElementById("projects");
+let hamburger = document.getElementById("hamburger");
 let isClick = 1;
 function togglemenu() {
   if (isClick % 2 != 0) {
@@ -9,11 +10,15 @@ function togglemenu() {
     hero.style.filter = "blur(3px)";
     footer.style.filter = "blur(3px)";
     projects.style.filter = "blur(3px)";
+    hamburger.style.backgroundColor = "black";
+    hamburger.style.color = "white";
   } else {
     links.style.display = "none";
     hero.style.filter = "blur(0px)";
     footer.style.filter = "blur(0px)";
     projects.style.filter = "blur(0px)";
+    hamburger.style.backgroundColor = "transparent";
+    hamburger.style.color = "black";
   }
   isClick += 1;
 }
